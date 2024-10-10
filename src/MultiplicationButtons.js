0d034ef905bb090a5d4e6c3b7d4fab6d5de800f0
hssw24 <hssw@gmx.de>
2024-10-10 10:29:13 +0200

    Beschreibung der Änderungen

--- src/MultiplicationButtons.js
+++ src/MultiplicationButtons.js
@@ -37,7 +37,7 @@ const styles = {
     backgroundColor: '#4CAF50',
     color: 'white',
     border: 'none',
-    width: '78px', // 70 Kleinere Breite, damit sie auf den Bildschirm passen
+    width: '75px', // 70 Kleinere Breite, damit sie auf den Bildschirm passen
     textAlign: 'center',
   },
 };
